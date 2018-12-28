4c8042d50fde2a0eb7ac127a2a0ba0b0227e502f
Joel Peña <joelpenov@gmail.com>
2018-12-28 18:57:34 +0000

    Add tooling, serving a file

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-var express = require('express');
+const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 const favicon = require('serve-favicon');
 
-const PORT = 3000;
+const { PORT } = process.env;
 const ROOT = __dirname;
-var server = express();
-
+const MODULES = 'node_modules';
+const server = express();
+// ./node_modules/.bin/eslint  app.js --fix
 server.use(morgan('tiny'));
 
 server.use(favicon(path.join(ROOT, 'favicon.ico')));
 server.use(express.static(path.join(ROOT, 'static')));
-server.use('/css', express.static(path.join(ROOT, 'node_modules/bootstrap/dist/css/')));
-server.use('/js', express.static(path.join(ROOT, 'node_modules/bootstrap/dist/js/')));
-server.use('/js', express.static(path.join(ROOT, 'node_modules/jquery/dist/')));
+server.use('/css', express.static(path.join(ROOT, `${MODULES}/bootstrap/dist/css/`)));
+server.use('/js', express.static(path.join(ROOT, `${MODULES}//bootstrap/dist/js/`)));
+server.use('/js', express.static(path.join(ROOT, `${MODULES}//jquery/dist/`)));
 
-server.get('/', function(req, resp){
-    debug(`${req.method} ${req.url}`)
-    resp.sendFile(path.join(__dirname, 'views','index.html'));
+server.get('/', (req, resp) => {
+  debug(`${req.method} ${req.url}`);
+  resp.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-server.listen(3000, function(){
-    debug(`Serving app on port ${chalk.green(PORT)}`);
-})
+server.listen(3000, () => {
+  debug(`Serving app on port ${chalk.green(PORT)}`);
+});
