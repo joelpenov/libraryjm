ac522d301436e54054e28160a0d45c7e5e9be4b0
Joel Peña <joelpenov@gmail.com>
2018-12-28 22:36:41 +0000

    Add views engine (EJS), set template: https://blackrockdigital.github.io/startbootstrap-modern-business/

--- app.js
+++ app.js
@@ -13,16 +13,20 @@ const server = express();
 server.use(morgan('tiny'));
 
 server.use(favicon(path.join(ROOT, 'favicon.ico')));
+
 server.use(express.static(path.join(ROOT, 'static')));
 server.use('/css', express.static(path.join(ROOT, `${MODULES}/bootstrap/dist/css/`)));
 server.use('/js', express.static(path.join(ROOT, `${MODULES}//bootstrap/dist/js/`)));
 server.use('/js', express.static(path.join(ROOT, `${MODULES}//jquery/dist/`)));
 
+server.set('views', path.join('./src/views'));
+server.set('view engine', 'ejs');
+
 server.get('/', (req, resp) => {
   debug(`${req.method} ${req.url}`);
-  resp.sendFile(path.join(__dirname, 'views', 'index.html'));
+  resp.render('index', { names: ['Ariel', 'Manolo', 'Elvis'], title: 'Cuesta Library' });
 });
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
   debug(`Serving app on port ${chalk.green(PORT)}`);
 });
